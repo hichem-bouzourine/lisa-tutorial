e7a88dd5b06edf6476883c4e8ce80fe72ccf4b4f
Hichem BOUZOURINE <hichembouzourine.uni@gmail.com>
2025-03-18 01:25:57 +0100

    Projet: a first correct solution along with explanation.

--- README.md
+++ README.md
@@ -1,13 +1,36 @@
-# LiSA tutorials
+# Explanation of Base Non-relational value domain (Intervals with rounding):
 
-This repository contains the code developed and used during LiSA's tutorials. Every tutorial is listed together with a link to the slides used and the tag of the code shown during the tutorial itself.
+## Class code Explanation:
+- **IntervallesWithRounding**: This class is the base
+class for a non-relational value domains. It is an abstract class
+that defines the interface for all non-relational value domains that have rounding.
 
-## Tutorials given
+in my code I've defined a helping class (`IntOrInf`) that represents an integer or infinity.
+and it extends the representation of `+∞` and `-∞`, there I define the rounding function that rounds 
+the value with a given precision.
 
-- PLDI '24 [[slides]](https://docs.google.com/presentation/d/1-oFl5Lgg-6mu0IdXMv8u-9w_ypc1aYbg-t_t8HVQBjw/edit?usp=sharing) [[code used]](https://github.com/lisa-analyzer/lisa-tutorial/releases/tag/pldi24)
+in the core class we find a TOP value that represents the top of the domain , and a BOTTOM value 
+that represents the bottom of the domain that I respectively defined as `+∞` and `-∞`. and I've defined a 
+default precision of 2 for the rounding function.
 
-- Lipari Summer School '24 [[slides]](https://docs.google.com/presentation/d/16MYOHTZJuuzuym9tcIH4L2r24Kn11vjAq7vpyTGcv14/edit?usp=sharing) [[code used]](https://github.com/lisa-analyzer/lisa-tutorial/releases/tag/lipari24)
+our least-upper-bottom is defined as the interval with the minimum lower bound and the maximum upper bound.
+our greatest-lower-bound is defined as the interval with the maximum lower bound and the minimum upper bound.
 
-- Ca' Foscari PhD Course - [[code used]](https://github.com/lisa-analyzer/lisa-tutorial/releases/tag/ssv24)
+our intervals are represented as a tuple of two `IntOrInf` values: `[a .. b]` where `a` is the lower bound and `b` is the upper bound.
 
-- Seminar at University of Verona - [[code used]](https://github.com/lisa-analyzer/lisa-tutorial/releases/tag/univr25)
+the Add, sub, mul, and div operations are defined as the following:
+ - [a .. b] + [c .. d] = [a + c .. b + d]
+ - [a .. b] - [c .. d] = [a - d .. b - c]
+ - [a .. b] * [c .. d] = [min(a * c, a * d, b * c, b * d) .. max(a * c, a * d, b * c, b * d)]
+ - [a .. b] / [c .. d] = [min(a / c, a / d, b / c, b / d) .. max(a / c, a / d, b / c, b / d)]
+
+## evalBinaryOperator : 
+ - evalBinaryOperator is a method that evaluates the binary operations on the intervals with rounding. 
+it checks if the left or right intervals are top or bottom and returns the appropriate value. 
+then it checks the operator type and calls the appropriate method to evaluate the operation.
+
+## evalNonNullConstant : 
+ - it evaluates the constant value and returns the interval with the rounded value of the constant.
+
+## assumeBinaryExpression :
+ - it assumes the binary expression and returns the environment with the new state of the variable.
--- src/main/java/it/unive/lisa/tutorial/IntervallesWithRounding.java
+++ src/main/java/it/unive/lisa/tutorial/IntervallesWithRounding.java
@@ -16,38 +16,33 @@ import it.unive.lisa.symbolic.value.operator.binary.BinaryOperator;
 import it.unive.lisa.util.representation.StringRepresentation;
 import it.unive.lisa.util.representation.StructuredRepresentation;
 
-import java.util.Set;
-
 public class IntervallesWithRounding implements BaseNonRelationalValueDomain<IntervallesWithRounding> {
-    public static final IntervallesWithRounding BOTTOM = new IntervallesWithRounding(Double.MIN_VALUE,
-            Double.MAX_VALUE);
-    public static final IntervallesWithRounding TOP = new IntervallesWithRounding(Double.MIN_VALUE,
-            Double.MAX_VALUE);
+    public static final IntervallesWithRounding TOP = new IntervallesWithRounding(IntOrInf.negativeInfinity(),
+            IntOrInf.positiveInfinity());
+    public static final IntervallesWithRounding BOTTOM = new IntervallesWithRounding(IntOrInf.positiveInfinity(),
+            IntOrInf.negativeInfinity());
 
-    private final double min, max;
+    private final IntOrInf min, max;
+    private static final int PRECISION = 2; // Number of decimal places for rounding
 
-    Set<Double> values;
-
-    private IntervallesWithRounding(double min, double max) {
+    private IntervallesWithRounding(IntOrInf min, IntOrInf max) {
         this.min = min;
         this.max = max;
     }
 
     @Override
-    public IntervallesWithRounding lubAux(IntervallesWithRounding intervallesWithRounding) throws SemanticException {
-        return new IntervallesWithRounding(Math.min(this.min, intervallesWithRounding.min),
-                Math.max(this.max, intervallesWithRounding.max));
+    public IntervallesWithRounding lubAux(IntervallesWithRounding other) throws SemanticException {
+        return new IntervallesWithRounding(IntOrInf.min(this.min, other.min), IntOrInf.max(this.max, other.max));
     }
 
     @Override
     public IntervallesWithRounding glbAux(IntervallesWithRounding other) throws SemanticException {
-        return new IntervallesWithRounding(Math.max(this.min, other.min),
-                Math.min(this.max, other.max));
+        return new IntervallesWithRounding(IntOrInf.max(this.min, other.min), IntOrInf.min(this.max, other.max));
     }
 
     @Override
     public boolean lessOrEqualAux(IntervallesWithRounding other) throws SemanticException {
-        return this.min <= other.min && this.max >= other.max;
+        return this.min.greaterOrEqual(other.min) && this.max.lessOrEqual(other.max);
     }
 
     @Override
@@ -62,12 +57,12 @@ public class IntervallesWithRounding implements BaseNonRelationalValueDomain<Int
 
     @Override
     public boolean isBottom() {
-        return this.min == Double.MIN_VALUE;
+        return this.min.greaterThan(this.max);
     }
 
     @Override
     public boolean isTop() {
-        return this.max == Double.MAX_VALUE;
+        return this.min.isNegativeInfinity() && this.max.isPositiveInfinity();
     }
 
     @Override
@@ -76,73 +71,58 @@ public class IntervallesWithRounding implements BaseNonRelationalValueDomain<Int
     }
 
     public IntervallesWithRounding add(IntervallesWithRounding other) {
-        // [a, b] + [c, d] = [a + c, b + d]
-        double newMin = this.min + other.min;
-        double newMax = this.max + other.max;
+        IntOrInf newMin = IntOrInf.add(this.min, other.min).round(PRECISION);
+        IntOrInf newMax = IntOrInf.add(this.max, other.max).round(PRECISION);
         return new IntervallesWithRounding(newMin, newMax);
     }
 
     public IntervallesWithRounding subtract(IntervallesWithRounding other) {
-        // [a, b] - [c, d] = [a - d, b - c] ------------------ ! à vérifier
-        double newMin = this.min - other.max;
-        double newMax = this.max - other.min;
+        // [a .. b] - [c .. d] = [a - d .. b - c]
+        IntOrInf newMin = IntOrInf.subtract(this.min, other.max).round(PRECISION);
+        IntOrInf newMax = IntOrInf.subtract(this.max, other.min).round(PRECISION);
         return new IntervallesWithRounding(newMin, newMax);
     }
 
     public IntervallesWithRounding multiply(IntervallesWithRounding other) {
-        // [a, b] * [c, d] = [min(ac, ad, bc, bd), max(ac, ad, bc, bd)]
-        double[] bounds = {
-                this.min * other.min,
-                this.min * other.max,
-                this.max * other.min,
-                this.max * other.max
+        IntOrInf[] bounds = { // [a .. b] * [c .. d] = [min(ac, ad, bc, bd) .. max(ac, ad, bc, bd)]
+                IntOrInf.multiply(this.min, other.min).round(PRECISION),
+                IntOrInf.multiply(this.min, other.max).round(PRECISION),
+                IntOrInf.multiply(this.max, other.min).round(PRECISION),
+                IntOrInf.multiply(this.max, other.max).round(PRECISION)
         };
-        double newMin = Math.min(Math.min(bounds[0], bounds[1]), Math.min(bounds[2],
-                bounds[3]));
-        double newMax = Math.max(Math.max(bounds[0], bounds[1]), Math.max(bounds[2],
-                bounds[3]));
+        IntOrInf newMin = IntOrInf.min(bounds[0], IntOrInf.min(bounds[1], IntOrInf.min(bounds[2], bounds[3])));
+        IntOrInf newMax = IntOrInf.max(bounds[0], IntOrInf.max(bounds[1], IntOrInf.max(bounds[2], bounds[3])));
         return new IntervallesWithRounding(newMin, newMax);
     }
 
     public IntervallesWithRounding divide(IntervallesWithRounding other) throws SemanticException {
-        if (other.min <= 0 && other.max >= 0) {
-            throw new SemanticException("Division by an interval containing zero is undefined.");
-        }
-        double[] bounds = {
-                round(this.min / other.min),
-                round(this.min / other.max),
-                round(this.max / other.min),
-                round(this.max / other.max)
+        // if (other.min.value <= 0 && other.max.value >= 0) {
+        // throw new SemanticException("Division by an interval containing zero is
+        // undefined.");
+        // }
+        IntOrInf[] bounds = { // [a .. b] / [c .. d] = { a/c, a/d, b/c, b/d }
+                IntOrInf.divide(this.min, other.min).round(PRECISION),
+                IntOrInf.divide(this.min, other.max).round(PRECISION),
+                IntOrInf.divide(this.max, other.min).round(PRECISION),
+                IntOrInf.divide(this.max, other.max).round(PRECISION)
         };
-        double newMin = Math.min(Math.min(bounds[0], bounds[1]), Math.min(bounds[2],
-                bounds[3]));
-        double newMax = Math.max(Math.max(bounds[0], bounds[1]), Math.max(bounds[2],
-                bounds[3]));
+        IntOrInf newMin = IntOrInf.min(bounds[0], IntOrInf.min(bounds[1], IntOrInf.min(bounds[2], bounds[3])));
+        IntOrInf newMax = IntOrInf.max(bounds[0], IntOrInf.max(bounds[1], IntOrInf.max(bounds[2], bounds[3])));
         return new IntervallesWithRounding(newMin, newMax);
-    }
-
-    public IntervallesWithRounding round() {
-        double newMin = Math.round(this.min);
-        double newMax = Math.round(this.max);
-        return new IntervallesWithRounding(newMin, newMax);
-    }
-
-    private double round(double value) {
-        if (Double.isInfinite(value) || Double.isNaN(value)) {
-            return value;
-        }
-        return Math.round(value);
     }
 
     @Override
     public IntervallesWithRounding evalBinaryExpression(BinaryOperator operator,
             IntervallesWithRounding left,
-            IntervallesWithRounding right, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
+            IntervallesWithRounding right,
+            ProgramPoint pp,
+            SemanticOracle oracle) throws SemanticException {
+
+        if (left.isBottom() || right.isBottom())
+            return bottom();
 
         if (left.isTop() || right.isTop())
             return top();
-        if (left.isBottom() || right.isBottom())
-            return bottom();
 
         if (operator instanceof AdditionOperator) {
             return left.add(right);
@@ -153,17 +133,18 @@ public class IntervallesWithRounding implements BaseNonRelationalValueDomain<Int
         } else if (operator instanceof DivisionOperator) {
             return left.divide(right);
         }
-        return BaseNonRelationalValueDomain.super.evalBinaryExpression(operator,
-                left, right, pp, oracle);
+        return top();
     }
 
     @Override
     public IntervallesWithRounding evalNonNullConstant(Constant constant, ProgramPoint pp, SemanticOracle oracle)
             throws SemanticException {
-        if (constant.getValue() instanceof Integer)
-            return new IntervallesWithRounding((Integer) constant.getValue(), (Integer) constant.getValue());
-
-        return BaseNonRelationalValueDomain.super.evalNonNullConstant(constant, pp, oracle);
+        if (constant.getValue() instanceof Number) {
+            double value = ((Number) constant.getValue()).doubleValue();
+            return new IntervallesWithRounding(new IntOrInf(value).round(PRECISION),
+                    new IntOrInf(value).round(PRECISION));
+        }
+        return top();
     }
 
     @Override
@@ -172,22 +153,151 @@ public class IntervallesWithRounding implements BaseNonRelationalValueDomain<Int
             ValueExpression right, ProgramPoint src, ProgramPoint dest, SemanticOracle oracle)
             throws SemanticException {
 
-        if (left instanceof Variable) {
+        if (left instanceof Variable && right instanceof Constant) {
             Variable x = (Variable) left;
-            if (right instanceof Constant) {
-                Constant y = (Constant) right;
-                if (y.getValue() instanceof Integer) {
-                    Number value = (Number) y.getValue();
-                    int doubleValue = value.intValue();
-                    environment.putState(x, new IntervallesWithRounding(doubleValue, doubleValue));
-
-                    return environment;
-                }
+            Constant y = (Constant) right;
+            if (y.getValue() instanceof Number) {
+                double value = ((Number) y.getValue()).doubleValue();
+                environment.putState(x, new IntervallesWithRounding(new IntOrInf(value).round(PRECISION),
+                        new IntOrInf(value).round(PRECISION)));
+                return environment;
             }
         }
-
-        return BaseNonRelationalValueDomain.super.assumeBinaryExpression(environment, operator, left, right, src, dest,
-                oracle);
+        return environment;
     }
 
+    private static class IntOrInf {
+        private final double value;
+        private final boolean isPositiveInfinity;
+        private final boolean isNegativeInfinity;
+
+        private IntOrInf(double value) {
+            this.value = value;
+            this.isPositiveInfinity = false;
+            this.isNegativeInfinity = false;
+        }
+
+        private IntOrInf(boolean isPositiveInfinity, boolean isNegativeInfinity) {
+            this.value = 0;
+            this.isPositiveInfinity = isPositiveInfinity;
+            this.isNegativeInfinity = isNegativeInfinity;
+        }
+
+        public static IntOrInf positiveInfinity() {
+            return new IntOrInf(true, false);
+        }
+
+        public static IntOrInf negativeInfinity() {
+            return new IntOrInf(false, true);
+        }
+
+        public boolean isPositiveInfinity() {
+            return isPositiveInfinity;
+        }
+
+        public boolean isNegativeInfinity() {
+            return isNegativeInfinity;
+        }
+
+        public boolean isFinite() {
+            return !isPositiveInfinity && !isNegativeInfinity;
+        }
+
+        public boolean greaterOrEqual(IntOrInf other) {
+            if (this.isPositiveInfinity)
+                return true;
+            if (other.isPositiveInfinity)
+                return false;
+            if (this.isNegativeInfinity)
+                return other.isNegativeInfinity;
+            if (other.isNegativeInfinity)
+                return true;
+            return this.value >= other.value;
+        }
+
+        public boolean lessOrEqual(IntOrInf other) {
+            if (this.isNegativeInfinity)
+                return true;
+            if (other.isNegativeInfinity)
+                return false;
+            if (this.isPositiveInfinity)
+                return other.isPositiveInfinity;
+            if (other.isPositiveInfinity)
+                return true;
+            return this.value <= other.value;
+        }
+
+        public boolean greaterThan(IntOrInf other) {
+            return !this.lessOrEqual(other);
+        }
+
+        public static IntOrInf min(IntOrInf a, IntOrInf b) {
+            if (a.isNegativeInfinity || b.isNegativeInfinity)
+                return negativeInfinity();
+            if (a.isPositiveInfinity)
+                return b;
+            if (b.isPositiveInfinity)
+                return a;
+            return new IntOrInf(Math.min(a.value, b.value));
+        }
+
+        public static IntOrInf max(IntOrInf a, IntOrInf b) {
+            if (a.isPositiveInfinity || b.isPositiveInfinity)
+                return positiveInfinity();
+            if (a.isNegativeInfinity)
+                return b;
+            if (b.isNegativeInfinity)
+                return a;
+            return new IntOrInf(Math.max(a.value, b.value));
+        }
+
+        public static IntOrInf add(IntOrInf a, IntOrInf b) {
+            if (a.isNegativeInfinity || b.isNegativeInfinity)
+                return negativeInfinity();
+            if (a.isPositiveInfinity || b.isPositiveInfinity)
+                return positiveInfinity();
+            return new IntOrInf(a.value + b.value);
+        }
+
+        public static IntOrInf subtract(IntOrInf a, IntOrInf b) {
+            if (a.isNegativeInfinity || b.isPositiveInfinity)
+                return negativeInfinity();
+            if (a.isPositiveInfinity || b.isNegativeInfinity)
+                return positiveInfinity();
+            return new IntOrInf(a.value - b.value);
+        }
+
+        public static IntOrInf multiply(IntOrInf a, IntOrInf b) {
+            if (a.isNegativeInfinity || b.isNegativeInfinity)
+                return positiveInfinity();
+            if (a.isPositiveInfinity || b.isPositiveInfinity)
+                return positiveInfinity();
+            return new IntOrInf(a.value * b.value);
+        }
+
+        public static IntOrInf divide(IntOrInf a, IntOrInf b) {
+            if (a.isNegativeInfinity || b.isNegativeInfinity)
+                return positiveInfinity();
+            if (a.isPositiveInfinity || b.isPositiveInfinity)
+                return positiveInfinity();
+            return new IntOrInf(a.value / b.value);
+        }
+
+        public IntOrInf round(int precision) {
+            if (isPositiveInfinity || isNegativeInfinity)
+                return this;
+            double scale = Math.pow(10, precision);
+            double roundedValue = Math.round(value * scale) / scale;
+            return new IntOrInf(roundedValue);
+        }
+
+        @Override
+        public String toString() {
+            if (isPositiveInfinity)
+                return "+∞";
+            if (isNegativeInfinity)
+                return "-∞";
+            return Double.toString(value);
+        }
+    }
 }
